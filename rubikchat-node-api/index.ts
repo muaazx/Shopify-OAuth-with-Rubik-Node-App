bb8218dbbc2e286cac517a427f92da5190f048ea
muaazx <amuaaz872@gmail.com>
2026-07-31 02:08:29 -0700

    fix: replace asterisk wildcard with regex in app.options to fix path-to-regexp crash in Express 5

--- rubikchat-node-api/index.ts
+++ rubikchat-node-api/index.ts
@@ -19,7 +19,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'x-shop'],
   credentials: true,
 }));
-app.options('*', cors());
+app.options(/(.*)/, cors());
 
 app.use(cookieParser());
 app.use(express.json());
